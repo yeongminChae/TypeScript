48bc349c67b47c556a0e7a15d47ce905505dfe14
Yeongmin_Chae <codudals98@naver.com>
2022-07-06 10:45:47 +0900

    work with typescript and javascript , JSDoc it allow us using typescript's protection in javascript

--- TS_Project/src/myPackage.js
+++ TS_Project/src/myPackage.js
@@ -1,6 +1,20 @@
+// @ts-checi
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
 export function init(config) {
   return true;
 }
+
+/**
+ *
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
   return code + 1;
 }
