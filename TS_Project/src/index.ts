73f4f7c2d0dd13c62997b0dba750b0f4dd005896
Yeongmin_Chae <codudals98@naver.com>
2022-07-06 10:14:24 +0900

    what is Lib configuration (You can specify which API is used and which environment executes the code for the typescrip ) and Declaration Files (it makes typescript can use the javascript files )

--- TS_Project/src/index.ts
+++ TS_Project/src/index.ts
@@ -1,6 +1,8 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
-  }
-}
+import { init, exit } from "myPackage";
+
+init({
+  url: "true",
+});
+exit(1);
+
+localStorage.clear();
