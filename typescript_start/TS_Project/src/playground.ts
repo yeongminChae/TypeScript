a457ff947c963dc5b001eb7af919e41d49d3bab7
Yeongmin_Chae <codudals98@naver.com>
2022-12-09 15:10:34 +0900

    update

---
+++ typescript_start/TS_Project/src/playground.ts
@@ -0,0 +1,15 @@
+interface User {
+  name: string;
+  id: number;
+}
+
+class UserAccount {
+  name: string;
+  id: number;
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const user: User = new UserAccount("murphy", 1);
